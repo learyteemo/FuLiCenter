bf467764b507e5d6a2dd24ab691ae0b1c10369ea
learyteemo <1604585139@qq.com>
2016-10-14 15:12:58 +0800

    在SplshActivity中实现闪屏等待两秒跳转到MainActivity

---
+++ app/src/main/java/cn/ucai/filicenter/activity/SplshActivity.java
@@ -0,0 +1,41 @@
+package cn.ucai.filicenter.activity;
+
+import android.content.Intent;
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cn.ucai.filicenter.R;
+
+public class SplshActivity extends AppCompatActivity {
+    static final long sleepTime = 2000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splsh);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+               long startTime =  System.currentTimeMillis();
+                //creat db
+                long lastTime = System.currentTimeMillis();
+                long cost = lastTime - startTime;
+                if (cost>sleepTime){
+
+                    try{
+                        Thread.sleep(2000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    startActivity(new Intent(SplshActivity.this,MainActivity.class));
+                }
+            }
+        }).start();
+    }
+}
