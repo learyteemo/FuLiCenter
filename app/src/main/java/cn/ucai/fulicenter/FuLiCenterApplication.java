c1525c2f238db950b318eb3720a68da9f6b4ea28
learyteemo <1604585139@qq.com>
2016-10-17 20:04:31 +0800

    修正Appliction

--- app/src/main/java/cn/ucai/fulicenter/FuLiCenterApplication.java
+++ app/src/main/java/cn/ucai/fulicenter/FuLiCenterApplication.java
@@ -1,16 +1,18 @@
 package cn.ucai.fulicenter;
 
 import android.app.Application;
-
 /**
  * Created by Administrator on 2016/10/17.
  */
 public class FuLiCenterApplication extends Application {
     private static FuLiCenterApplication instance;
-    public FuLiCenterApplication() {
+    public static FuLiCenterApplication application;
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        application = this;
         instance = this;
     }
-
     public static FuLiCenterApplication getInstance() {
         if (instance == null){
             instance = new FuLiCenterApplication();
